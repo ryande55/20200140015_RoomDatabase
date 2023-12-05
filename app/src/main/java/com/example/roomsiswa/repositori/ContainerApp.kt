32bb02188ca130d7c2c09cf28de01399bce8120e
ryande55 <buatgithub23@gmail.com>
2023-12-05 14:34:30 +0700

    21

--- app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
@@ -7,8 +7,3 @@ interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
-class ContainerDataApp(private val context: Context): ContainerApp{
-    override val repositoriSiswa: RepositoriSiswa by lazy {
-        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
-    }
-}
