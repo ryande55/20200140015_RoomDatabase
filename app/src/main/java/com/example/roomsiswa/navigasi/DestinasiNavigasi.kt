7e107e0a2cede1fe39da28773da4eebbad893c87
ryande55 <buatgithub23@gmail.com>
2023-12-05 14:33:31 +0700

    18

--- app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -4,4 +4,10 @@ interface DestinasiNavigasi {
     /**
      *  Nama unik untuk menentukan jalur untuk composable
      */
-    
+    val route: String
+
+    /**
+     *  String resource id yang berisi judul akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
+}
