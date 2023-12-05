87cd0cf59ee650d89a18fe106af2f5d59510e427
ryande55 <buatgithub23@gmail.com>
2023-12-05 14:38:02 +0700

    32

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -10,8 +10,4 @@ class AplikasiSiswa : Application() {
      */
     lateinit var container: ContainerApp
 
-    override fun onCreate() {
-        super.onCreate()
-        container = ContainerDataApp(this)
-    }
-}
+    
