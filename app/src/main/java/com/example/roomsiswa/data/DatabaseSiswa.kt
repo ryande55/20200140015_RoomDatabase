beb7666922024023b63b00f0ece50cfb2c42f5ff
ryande55 <buatgithub23@gmail.com>
2023-12-05 14:27:17 +0700

    2

--- app/src/main/java/com/example/roomsiswa/data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/data/DatabaseSiswa.kt
@@ -7,5 +7,10 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase(){
-    
-}
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        
