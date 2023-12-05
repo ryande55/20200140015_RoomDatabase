7eb7111f20f17635301726dd2d0bd145c7e1b0e6
ryande55 <buatgithub23@gmail.com>
2023-12-05 14:32:09 +0700

    13

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -14,15 +14,4 @@ class HomeViewModel(private val  repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
     }
-    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
-        .filterNotNull()
-        .map { HomeUiState(listSiswa = it.toList()) }
-        .stateIn(scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
-            initialValue = HomeUiState()
-        )
-
-    data class  HomeUiState(
-        val listSiswa: List<Siswa> = listOf()
-    )
-}
+    
